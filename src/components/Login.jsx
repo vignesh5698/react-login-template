100f57988d01f293332f9461b8f014becd64fe22
vignesh5698 <vigneshit13@gmail.com>
2021-02-15 10:26:42 +0530

    Added signup form

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { Form, Input, Button, Checkbox } from 'antd';
-import { UserOutlined, LockOutlined } from '@ant-design/icons';
+import { UserOutlined, LockOutlined, MailOutlined, PhoneOutlined } from '@ant-design/icons';
 import { Tabs, Card } from 'antd';
 
 const { TabPane } = Tabs;
@@ -10,6 +10,21 @@ class Login extends Component {
 
 	onFinish = () => {
 
+	}
+
+	validatePassword = (rule, value, callback) => {
+		if (value.length < 6) {
+			callback("Password must have atleast 6 characters in length");
+    } else {
+      callback();
+    }
+	}
+
+	validateMobileNumber = (rule, value, callback) => {
+		if(value.length === 10) {
+			callback();
+		}
+		callback('Invalid mobile number');
 	}
 
 	renderLoginForm = () => {
@@ -66,8 +81,29 @@ class Login extends Component {
 					<Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
 				</Form.Item>
 				<Form.Item
+					name="email"
+					rules={[
+						{required: true, message: 'Please input your Email address!'},
+						{type: "email"}
+					]}
+				>
+					<Input prefix={<MailOutlined className="site-form-item-icon" />} placeholder="Email"/>
+				</Form.Item>
+				<Form.Item
+					name="phone"
+					rules={[
+						{required: true, message: 'Please input your mobile number!'},
+						{validator: this.validateMobileNumber}
+					]}
+				>
+					<Input prefix={<PhoneOutlined className="site-form-item-icon" />} placeholder="Mobile Number"/>
+				</Form.Item>
+				<Form.Item
 					name="password"
-					rules={[{ required: true, message: 'Please input your Password!' }]}
+					rules={[
+						{required: true, message: 'Please input your Password!'},
+						{validator: this.validatePassword}
+					]}
 				>
 					<Input
 						prefix={<LockOutlined className="site-form-item-icon" />}
@@ -75,12 +111,16 @@ class Login extends Component {
 						placeholder="Password"
 					/>
 				</Form.Item>
-				<Form.Item>
-					<Form.Item name="remember" valuePropName="checked" noStyle>
-						<Checkbox>Remember me</Checkbox>
-					</Form.Item>
+				<Form.Item
+					name="repeatPassword"
+					rules={[{ required: true, message: 'Please input your Password!' }]}
+				>
+					<Input
+						prefix={<LockOutlined className="site-form-item-icon" />}
+						type="password"
+						placeholder="Repeat Password"
+					/>
 				</Form.Item>
-
 				<Form.Item>
 					<Button type="primary" htmlType="submit" className="login-form-button">
 						Signup
